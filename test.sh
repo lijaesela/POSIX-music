76bf9aacfc40c06e64461180dc0f059b225b531e
lijaesela <janponalukasu@tutanota.com>
2020-08-11 17:21:59 -0500

    made the example more helpful and exciting

--- test.sh
+++ test.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+
+# source "base.sh" to import all of the tools we need
 . ./base.sh
 
+# set BPM and time signature
 bpm=100
 timesig="4/4"
+# set "calscale", the decimal place that bc will round to.
+# higher numbers will give more accurate rhythms.
 calcscale=8
 
-init_rhythm
+# call this function to set all of the "note" variables
 init_pitch
+# call this one to set all of the time variables
+init_rhythm
 
 echo "Starting!"
 
@@ -86,5 +93,70 @@ pr $note29 $eighth
 
 r $quarter
 
+# tempo change!
+bpm=140
+init_rhythm
+
+echo "Getting faster!"
+
+#
+I &
+#
+pr $note15 $eighth
+pr $note17 $eighth
+
+pr $note20 $eighth
+pr $note24 $eighth
+
+r $eighth
+pr $note24 $eighth
+
+pr $note20 $eighth
+pr $note17 $eighth
+
+#
+I &
+#
+pr $note15 $eighth
+pr $note17 $eighth
+
+pr $note20 $eighth
+pr $note24 $eighth
+
+r $eighth
+pr $note24 $eighth
+
+pr $note20 $eighth
+pr $note17 $eighth
+
+#
+ii &
+#
+pr $note17 $eighth
+pr $note18 $eighth
+
+pr $note22 $eighth
+pr $note25 $eighth
+
+r $quarter
+
+r $quarter
+
+#
+ii &
+#
+pr $note17 $eighth
+pr $note18 $eighth
+
+pr $note22 $eighth
+pr $note25 $eighth
+
+r $eighth
+pr $note29 $eighth
+
+r $quarter
+
 #
 p $note1
+
+echo "Done."
